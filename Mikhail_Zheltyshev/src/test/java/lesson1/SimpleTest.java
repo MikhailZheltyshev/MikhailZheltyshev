fce45bbcb3bd4bb5ca4bbeed450048d260865a20
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-10-11 19:02:13 +0300

    First simple test created
    Checked for successful driver attachment and first test run

---
+++ Mikhail_Zheltyshev/src/test/java/lesson1/SimpleTest.java
@@ -0,0 +1,15 @@
+package lesson1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void simpleTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+        //driver.close();
+    }
+}
