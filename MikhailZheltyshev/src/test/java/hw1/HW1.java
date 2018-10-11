2483300a0fd42380b4d625b6f724731317322afd
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-10-11 23:51:10 +0300

    Up to 8th Step is done

--- MikhailZheltyshev/src/test/java/hw1/HW1.java
+++ MikhailZheltyshev/src/test/java/hw1/HW1.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.setProperty;
@@ -41,7 +42,41 @@ public class HW1 {
         //5 Assert Browser Title
         assertEquals(driver.getTitle(), "Home Page");
 
+        //6 Assert Menu Buttons Are Displayed And Have Proper Text
+        //6.1 Get List of Menu Buttons
+        List<WebElement> upperToolbarItems = driver.findElements(By.cssSelector(".uui-navigation.nav.navbar-nav.m-l8 li"));
 
+        //6.2 Assert Browser Up-Level Buttons Are displayed
+        assertTrue(upperToolbarItems.get(0).isDisplayed());
+        assertTrue(upperToolbarItems.get(1).isDisplayed());
+        assertTrue(upperToolbarItems.get(2).isDisplayed());
+        assertTrue(upperToolbarItems.get(11).isDisplayed());
 
+        //6.3 Assert Browser Up-Level Buttons Have Proper Text
+        assertEquals(upperToolbarItems.get(0).getText(), "HOME");
+        assertEquals(upperToolbarItems.get(1).getText(), "CONTACT FORM");
+        assertEquals(upperToolbarItems.get(2).getText(), "SERVICE");
+        assertEquals(upperToolbarItems.get(11).getText(), "METALS & COLORS");
+
+        //7 Assert that 4 icons are presented and displayed on the index page
+        List<WebElement> benefitIconElements = driver.findElements(By.cssSelector(".benefit-icon"));
+        assertEquals(benefitIconElements.size(), 4);
+        for (WebElement icon: benefitIconElements){
+            assertTrue(icon.isDisplayed());
+        }
+
+        //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.size(), 4);
+        assertEquals(textsUnderIconsElements.get(0).getText(),"To include good practices\n" +
+                "and ideas from successful\n" +
+                "EPAM project");
+        assertEquals(textsUnderIconsElements.get(1).getText(),"To be flexible and\n" +
+                "customizable");
+        assertEquals(textsUnderIconsElements.get(2).getText(),"To be multiplatform");
+        assertEquals(textsUnderIconsElements.get(3).getText(),"Already have good base\n" +
+                "(about 20 internal and\n" +
+                "some external projects),\n" +
+                "wish to get more…");
     }
 }
