add268d1f8d24e000433cc873955e5d01e87af3c
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-10-15 22:10:41 +0300

    Prepared code basis (1-8 steps) for exercise 2

---
+++ MikhailZheltyshev/src/test/java/hw2/ex1/ex2/HomeWork2Exc2.java
@@ -0,0 +1,177 @@
+package hw2.ex1.ex2;
+
+import DataProviders.DataProviders;
+import base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+public class HomeWork2Exc2 extends TestBase {
+
+    @Test(groups = "Smoke", dataProvider = "checkTextsOnIndexDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexS1(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = "Smoke", dataProvider = "checkTextsOnIndexDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexS2(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = "Smoke", dataProvider = "checkTextsOnIndexDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexS3(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = "Regression", dataProvider = "checkTextsOnIndexDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexR1(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = "Regression", dataProvider = "checkTextsOnIndexDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexR2(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = "Regression", dataProvider = "checkTextsOnIndexDataProvider", dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexR3(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = {"Smoke", "Regression"} ,
+            dataProvider = "checkTextsOnIndexDataProvider",
+            dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexSR1(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = {"Smoke", "Regression"},
+            dataProvider = "checkTextsOnIndexDataProvider",
+            dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexSR2(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+
+    @Test(groups = {"Smoke", "Regression"},
+            dataProvider = "checkTextsOnIndexDataProvider",
+            dataProviderClass = DataProviders.class)
+    public void checkTextUnderTheImagesOfIndexSR3(int textNum, String textExpected) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        //1 Open test site by URL
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert that all texts under icons on Index page have proper text according to DataProvider data set
+        List<WebElement> textsUnderIconsElements = driver.findElements(By.cssSelector(".benefit-txt"));
+        assertEquals(textsUnderIconsElements.get(textNum).getText(),textExpected);
+
+        driver.close();
+    }
+}
+
