9e5863d8da242d0a1f10e21d213b9710266cd85e
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-10-17 22:37:09 +0300

    HW 3 draft

---
+++ MikhailZheltyshev/src/main/java/pageObjects/HomePage.java
@@ -0,0 +1,161 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.DataProvider;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class HomePage {
+    @FindBy(css = ".profile-photo")
+    private WebElement profileButton;
+
+    @FindBy(css = "[id = 'Name']")
+    private WebElement login;
+
+    @FindBy(css = "[id = 'Password']")
+    private WebElement password;
+
+    @FindBy(css = ".login [type = 'submit']")
+    private WebElement submit;
+
+    @FindBy(css = ".profile-photo span")
+    private WebElement loggedUserNameElement;
+
+    @FindBy(css = ".m-l8 > li")
+    private List <WebElement> upperToolbarItems;
+
+    @FindBy(css = ".benefit-icon")
+    private List<WebElement> benefitIconElements;
+
+    @FindBy(css = ".benefit-txt")
+    private List<WebElement> textsUnderIconsElements;
+
+    @FindBy(css = "h3.main-title.text-center")
+    private WebElement mainHeaderElement;
+
+    @FindBy(css = ".main-txt.text-center")
+    private WebElement mainSubHeader;
+
+    @FindBy(css = "iframe")
+    private WebElement iFrameElement;
+
+    @FindBy(css = "img[id='epam_logo']")
+    private WebElement epamLogo;
+
+    @FindBy(css = "[class='text-center']")
+    private WebElement defaultHeaderElement;
+
+    @FindBy(css = "[class='text-center'] a")
+    private WebElement linkToJdiGitHub;
+
+    @FindBy(css = ".uui-side-bar")
+    private WebElement navSideBarElement;
+
+    @FindBy(css = "footer")
+    private WebElement footerElement;
+
+    public void open(WebDriver driver) {
+        driver.get("https://epam.github.io/JDI/index.html");
+    }
+
+    public void checkTitle(WebDriver driver, String expectedTitle){
+        assertEquals(driver.getTitle(), expectedTitle);
+    }
+
+    public void login(String login, String password) {
+        profileButton.click();
+        this.login.sendKeys(login);
+        this.password.sendKeys(password);
+        submit.click();
+    }
+
+    public void checkLoggedUserName(String expectedName) {
+        assertTrue(loggedUserNameElement.isDisplayed());
+        assertEquals(loggedUserNameElement.getText(), expectedName);
+    }
+
+    public void checkUpperToolbarButtons() {
+
+        assertEquals(upperToolbarItems.size(), 4);
+        for (WebElement item: upperToolbarItems){
+            assertTrue(item.isDisplayed());
+        }
+
+        assertEquals(upperToolbarItems.get(0).getText(), "HOME");
+        assertEquals(upperToolbarItems.get(1).getText(), "CONTACT FORM");
+        assertEquals(upperToolbarItems.get(2).getText(), "SERVICE");
+        assertEquals(upperToolbarItems.get(3).getText(), "METALS & COLORS");
+    }
+
+    public void checkBenefitIcons() {
+        assertEquals(benefitIconElements.size(), 4);
+        for (WebElement icon: benefitIconElements){
+            assertTrue(icon.isDisplayed());
+        }
+    }
+
+    public void checkBenefitTexts() {
+        assertEquals(textsUnderIconsElements.size(), 4);
+        assertEquals(textsUnderIconsElements.get(0).getText(),"To include good practices\n" +
+                "and ideas from successful\n" +
+                "EPAM project");
+        assertEquals(textsUnderIconsElements.get(1).getText(),"To be flexible and\n" +
+                "customizable");
+        assertEquals(textsUnderIconsElements.get(2).getText(),"To be multiplatform");
+        assertEquals(textsUnderIconsElements.get(3).getText(),"Already have good base\n" +
+                "(about 20 internal and\n" +
+                "some external projects),\n" +
+                "wish to get more…");
+    }
+
+    public void checkMainHeaderText() {
+        String mainHeaderText = mainHeaderElement.getText();
+        assertEquals(mainHeaderText, "EPAM FRAMEWORK WISHES…");
+        String mainSubHeaderText = mainSubHeader.getText();
+        assertEquals(mainSubHeaderText, "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD" +
+                " TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION" +
+                " ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN" +
+                " VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
+    }
+
+    public void checkIframeIsDisplayed() {
+        assertTrue(iFrameElement.isDisplayed());
+    }
+
+    public void checkEpamLogoInIframe(WebDriver driver) {
+        driver.switchTo().frame("iframe");
+        assertTrue(epamLogo.isDisplayed());
+    }
+
+    public void switchBackToOriginalWindow(WebDriver driver) {
+        driver.switchTo().defaultContent();
+    }
+
+    public void checkSubHeaderText() {
+        String defaultHeaderText = defaultHeaderElement.getText();
+        assertEquals(defaultHeaderText, "JDI GITHUB");
+    }
+
+    public void checkJdiLink(String expectedUrl) {
+        assertTrue(linkToJdiGitHub.isDisplayed());
+        assertEquals(linkToJdiGitHub.getAttribute("href"), expectedUrl);
+    }
+
+    public void checkLeftSideBarIsDisplayed() {
+        assertTrue(navSideBarElement.isDisplayed());
+    }
+
+    public void checkFooterIsDisplayed() {
+        assertTrue(footerElement.isDisplayed());
+    }
+
+    public void close(WebDriver driver) {
+        driver.close();
+    }
+}
---
+++ MikhailZheltyshev/src/test/java/hw3/ContentPageTests.java
@@ -0,0 +1,80 @@
+package hw3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContentPageTests {
+
+    private WebDriver driver = new ChromeDriver();
+    private HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+
+    @BeforeMethod
+    public void beforeClass() {
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterMethod() {
+        driver.close();
+    }
+
+    @Test
+    public void testJdiIndexPage() {
+
+        //1 Open test site by URL
+        homePage.open(driver);
+
+        //2 Assert Browser Title
+        homePage.checkTitle(driver, "Home Page");
+
+        //3 Perform login
+        homePage.login("epam", "1234");
+
+        //4 Assert User name in the left-top side of screen that user is loggined
+        homePage.checkLoggedUserName("PITER CHAILOVSKII");
+
+        //5 Assert Browser Title
+        homePage.checkTitle(driver, "Home Page");
+
+        //6 Assert that there are 4 items on the header section are displayed and they have proper texts
+        homePage.checkUpperToolbarButtons();
+
+        //7 Assert that there are 4 images on the Index Page and they are displayed
+        homePage.checkBenefitIcons();
+
+        //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
+        homePage.checkBenefitTexts();
+
+        //9 Assert a text of the main header
+        homePage.checkMainHeaderText();
+
+        //10 Assertthat there is the iframe in the center of page
+        homePage.checkIframeIsDisplayed();
+
+        //11 Switch to the iframe and check that there is Epam logo in the left top conner of iframe
+        homePage.checkEpamLogoInIframe(driver);
+
+        //12 Switch to original window back
+        homePage.switchBackToOriginalWindow(driver);
+
+        //13 Assert a text of the sub header
+        homePage.checkSubHeaderText();
+
+        //14 Assert that JDI GITHUB is a link and has a proper URL
+        homePage.checkJdiLink("https://github.com/epam/JDI");
+
+        //15 Assert that there is Left Section
+        homePage.checkLeftSideBarIsDisplayed();
+
+        //16 Assert that there is Footer
+        homePage.checkFooterIsDisplayed();
+    }
+}
