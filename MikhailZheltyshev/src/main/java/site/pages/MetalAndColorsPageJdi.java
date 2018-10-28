437105e84a434041a96e15cc69655de715aed6d8
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-10-28 22:58:20 +0300

    HW8 Implemented handling of "colors", "metals" and "natures" page elements

--- MikhailZheltyshev/src/main/java/site/pages/MetalAndColorsPageJdi.java
+++ MikhailZheltyshev/src/main/java/site/pages/MetalAndColorsPageJdi.java
@@ -1,8 +1,55 @@
 package site.pages;
 
+import com.epam.jdi.uitests.core.interfaces.complex.ICheckList;
+import com.epam.jdi.uitests.core.interfaces.complex.IComboBox;
+import com.epam.jdi.uitests.core.interfaces.complex.IDropDown;
+import com.epam.jdi.uitests.core.interfaces.complex.IDropList;
+import com.epam.jdi.uitests.web.selenium.elements.complex.CheckList;
+import com.epam.jdi.uitests.web.selenium.elements.complex.Dropdown;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebPage;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.JFindBy;
 import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.JPage;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JComboBox;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JDropList;
+import com.epam.jdi.uitests.web.selenium.elements.pageobjects.annotations.objects.JDropdown;
+import enums.jdi.ColorsList;
+import enums.jdi.MetalsList;
+import enums.jdi.Nature;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 @JPage(url = "/metals-colors.html", title = "Metal and Colors")
 public class MetalAndColorsPageJdi extends WebPage {
+    @JDropdown(
+            jroot = @JFindBy(css = ".colors"),
+            jlist = @JFindBy(tagName = "li"),
+            jvalue = @JFindBy(css = ".filter-option"))
+    public IDropDown<ColorsList> colors;
+
+    @JDropdown(
+            jroot = @JFindBy(css = ".metals"),
+            jlist = @JFindBy(tagName = "li"),
+            jvalue = @JFindBy(css = ".filter-option"),
+            jexpand = @JFindBy(css = ".caret"))
+    public IDropDown<MetalsList> metals;
+
+    @JDropList(
+            jroot = @JFindBy(css = ".dropdown.salad"),
+            jlist = @JFindBy(tagName = "li"),
+            jexpand = @JFindBy(css = ".caret"),
+            jvalue = @JFindBy(tagName = "button")
+    )
+    public IDropList vegetables;
+
+    @FindBy(css = "#elements-checklist p")
+    public ICheckList<Nature> nature = new CheckList<Nature>() {
+        @Override
+        protected boolean isSelectedAction(WebElement el) {
+            return el.findElement(By.tagName("input")).getAttribute("checked") != null;
+        }
+    };
+
+
 }
+
--- MikhailZheltyshev/src/test/java/hw8/MetalsAndColorsPageTests.java
+++ MikhailZheltyshev/src/test/java/hw8/MetalsAndColorsPageTests.java
@@ -1,6 +1,5 @@
 package hw8;
 
-import enums.Users;
 import org.testng.annotations.Test;
 
 import static enums.Users.*;
@@ -9,14 +8,20 @@ import static site.JdiExampleSite.*;
 
 public class MetalsAndColorsPageTests extends MetalsAndColorsPageInit {
 
+
+
     @Test
-    public void loginTest() {
+    public void loginTest() throws InterruptedException {
         homePage.open();
         homePage.checkOpened();
         login();
         checkLoggedInUserName(PITER_CHAILOVSKII.displayName);
         openMetalAndColorsPageByHeader();
-        metalAndColorsPage.checkOpened();
+        metalAndColorsPage.colors.select("Blue");
+        metalAndColorsPage.metals.select("Gold");
+        //metalAndColorsPage.vegetables.select("Onion");
+        metalAndColorsPage.nature.select("Fire","Water");
+        Thread.sleep(10000);
 
     }
 }
