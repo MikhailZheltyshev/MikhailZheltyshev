fbf09f06ac785ad7a07caee69a8eb8c6070012ca
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-11-01 20:15:20 +0300

    HW4 stabilized slider position setting: proper rounding and taking into count slider's handle width

--- MikhailZheltyshev/src/main/java/utils/SliderHandler.java
+++ MikhailZheltyshev/src/main/java/utils/SliderHandler.java
@@ -10,34 +10,25 @@ public class SliderHandler {
 
     private SelenideElement sliderRange = $(".range");
 
-    private Double getStep() {
+    private SelenideElement sliderHandle = $(".ui-slider");
+
+    private Double getStep () {
         return (double) sliderRange.getSize().width / 100;
     }
 
-    public void setPosition(SelenideElement handle, int targetPosition) {
+    public void setPosition ( SelenideElement handle, int targetPosition ) {
         Actions actions = new Actions(getWebDriver());
-        Double current = getCurrentPosition(handle);
-        int xOffset = (int) ((targetPosition - current - 1) * getStep());
-        actions.dragAndDropBy(handle, xOffset, 0).perform();
 
+        double halfSliderWidth = handle.getSize().getWidth() / 2;
+        double sliderPosition = sliderHandle.getLocation().x;
+        double current = handle.getLocation().x + halfSliderWidth;
+        double xOffset = sliderPosition - current + targetPosition * getStep();
+
+        actions.dragAndDropBy(handle, (int) Math.ceil(xOffset), 0).perform();
     }
 
-    public Double getCurrentPosition(SelenideElement handle){
+    public Double getCurrentPosition ( SelenideElement handle ) {
         return Double.parseDouble(handle.getCssValue("left").replaceAll("px", "")) / getStep();
     }
 
-    //This method works more stable on Mac platform for some reason.
-    public void setPositionExperimental(SelenideElement handle, int targetPosition) {
-        Actions actions = new Actions(getWebDriver());
-        double current = Double.parseDouble(handle.getCssValue("left").replaceAll("px", ""));
-        int xOffset;
-        if (targetPosition == 0) {
-            xOffset = (int) -Math.ceil(current);
-        } else if (targetPosition == 100) {
-            xOffset = (int) Math.ceil(sliderRange.getSize().width - current);
-        } else {
-            xOffset = (int) ((targetPosition - (current / getStep()) - 1) * getStep());
-        }
-        actions.dragAndDropBy(handle, xOffset, 0).perform();
-    }
 }
