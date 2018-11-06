27401c9ae38edb2a5d308438e141c3858ebc206f
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-11-06 23:02:36 +0300

    appium-hw3 draft. Connection to the mf is set properly.

--- MikhailZheltyshev/src/main/java/appium_setup/Driver.java
+++ MikhailZheltyshev/src/main/java/appium_setup/Driver.java
@@ -19,6 +19,7 @@ public class Driver extends TestProperties {
     private static String TEST_PLATFORM;
     private static String DRIVER;
     private static String DEVICE_NAME;
+    private static String UDID;
 
     protected void prepareDriver() throws Exception {
         capabilities = new DesiredCapabilities();
@@ -31,11 +32,12 @@ public class Driver extends TestProperties {
         AUT = mobileAppName == null ? null : resourcePath + mobileAppName;
         System.out.println("aut=" + AUT); //Write Current AUT to console
         String t_sut = getProp("sut");
-        SUT = t_sut == null ? null : "http://" + t_sut;
+        SUT = t_sut == null ? null : "https://" + t_sut;
         System.out.println("sut=" + SUT); //Write Current SUT to console
         TEST_PLATFORM = getProp("platform");
         DRIVER = getProp("driver");
         DEVICE_NAME = getProp("devicename");
+        UDID = getProp("udid");
 
         //Check type of test platform to set proper browserName capability
         switch (TEST_PLATFORM) {
@@ -44,6 +46,7 @@ public class Driver extends TestProperties {
                 browserName = "Chrome";
                 break;
             case "iOS":
+                //capabilities.setCapability(MobileCapabilityType.UDID,UDID);
                 browserName = "Safari";
                 break;
             default:
@@ -69,6 +72,7 @@ public class Driver extends TestProperties {
         if (waitSingle == null) waitSingle = new WebDriverWait(driver(), 10);
 
     }
+
     //Method to access singleton
     protected AppiumDriver driver() throws Exception {
         if (driverSingle == null) prepareDriver();
