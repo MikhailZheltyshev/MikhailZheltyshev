3303a1f275477305330dc2aad804ec6b441adf7a
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-11-07 21:10:10 +0300

    HW5 Separated into classes

---
+++ MikhailZheltyshev/src/test/java/hw5/DatesPageTests.java
@@ -0,0 +1,72 @@
+package hw5;
+
+import base.SelenideTestBase;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import listeners.AllureAttachmentListener;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pageObjects.DatesPageSelenide;
+import pageObjects.HomePageSelenide;
+
+import static com.codeborne.selenide.Selenide.page;
+import static enums.Users.PITER_CHAILOVSKII;
+
+@Feature("UI Tests")
+@Story("Service And Dates Pages Testing")
+@Listeners(AllureAttachmentListener.class)
+public class DatesPageTests extends SelenideTestBase {
+
+    private HomePageSelenide homePage;
+    private DatesPageSelenide datesPage;
+
+    @BeforeClass
+    public void beforeClass() {
+        homePage = page(HomePageSelenide.class);
+        datesPage = page(DatesPageSelenide.class);
+    }
+
+    @Test
+    public void datesPageInterfaceCheck() {
+
+        //1 Open test site by URL
+        homePage.open();
+
+        //2 Assert Browser title
+        homePage.checkTitle();
+
+        //3 Perform login
+        homePage.login(PITER_CHAILOVSKII);
+
+        //4 Assert User name in the left-top side of screen that user is logged
+        homePage.checkLoggedUserName(PITER_CHAILOVSKII);
+
+        //5 Open through the header menu Service -> Dates Page
+        homePage.openDatesPageThroughTheHeaderMenu();
+
+        //6 Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most rigth position
+        datesPage.setSlidersPosition(0, 100);
+
+        //7 Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        datesPage.checkSlidersSettingLog(0, 100);
+
+        //8 Using drag-and-drop set Range sliders. left sliders - the most left position, right slider - the most left position.
+        datesPage.setSlidersPosition(0, 0);
+
+        //9 Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        datesPage.checkSlidersSettingLog(0, 0);
+
+        //10 Using drag-and-drop set Range sliders. left sliders - the most rigth position, right slider - the most rigth position.
+        datesPage.setSlidersPosition(100, 100);
+
+        //11 Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        datesPage.checkSlidersSettingLog(100, 100);
+
+        //12 Using drag-and-drop set Range sliders: from 30 to 70.
+        datesPage.setSlidersPosition(30, 70);
+
+        //13 Assert that for "From" and "To" sliders there are logs rows with corresponding values
+        datesPage.checkSlidersSettingLog(30, 70);
+    }
+}
---
+++ MikhailZheltyshev/src/test/java/hw5/ServicePageFailedTests.java
@@ -0,0 +1,90 @@
+package hw5;
+
+import base.SelenideTestBase;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import listeners.AllureAttachmentListener;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pageObjects.DifferentElementsPageSelenide;
+import pageObjects.HomePageSelenide;
+
+import static com.codeborne.selenide.Selenide.page;
+import static enums.DIfferentElementsPageCheckBoxes.*;
+import static enums.DifferentElementsPageDropDownItems.YELLOW;
+import static enums.DifferentElementsPageRadioButtons.SELEN;
+import static enums.Users.PITER_CHAILOVSKII;
+
+@Feature("UI Tests")
+@Story("Service And Dates Pages Testing")
+@Listeners(AllureAttachmentListener.class)
+public class ServicePageFailedTests extends SelenideTestBase {
+
+    private HomePageSelenide homePage;
+    private DifferentElementsPageSelenide differentElementsPage;
+
+    @BeforeClass
+    public void beforeClass() {
+        homePage = page(HomePageSelenide.class);
+        differentElementsPage = page(DifferentElementsPageSelenide.class);
+    }
+
+    @Test
+    public void servicePageInterfaceCheckFailed() {
+
+        //1 Open test site by URL
+        homePage.open();
+
+        //2 Assert Browser title
+        homePage.checkTitle();
+
+        //3 Perform login
+        homePage.login(PITER_CHAILOVSKII);
+
+        //4 Assert User name in the left-top side of screen that user is logged
+        homePage.checkLoggedUserName(PITER_CHAILOVSKII);
+
+        //5 Click on "Service" subcategory in the header and check that drop down contains options
+        homePage.checkUpperServiceMenuContent();
+
+        //6 Click on Service subcategory in the left section and check that drop down contains options
+        homePage.checkLeftServiceMenuContent();
+
+        //7 Open through the header menu Service -> Different Elements Page
+        homePage.openDifferentElementsPageThroughTheHeaderMenu();
+
+        //8 Check interface on Different elements page, it contains all needed elements
+        differentElementsPage.checkDifferentElementsPageContent();
+
+        //9 Assert that there is Right Section
+        differentElementsPage.checkRightSectionExists();
+
+        //10 Assert that there is Left Section
+        differentElementsPage.checkLeftSectionExists();
+
+        //11 Select checkboxes
+        differentElementsPage.selectCheckBoxes(EARTH, FIRE);
+
+        //12 Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox. 
+        differentElementsPage.checkLoggingOfCheckBoxesChecked(WATER, WIND);
+
+        //13 Select radio
+        differentElementsPage.selectRadioButton(SELEN);
+
+        //14 Assert that for radiobutton there is a log row and value is corresponded to the status of radiobutton. 
+        differentElementsPage.checkLoggingOfRadioButtons(SELEN);
+
+        //15 Select in dropdown
+        differentElementsPage.selectItemFromDropDownMenu(YELLOW);
+
+        //16 Assert that for dropdown there is a log row and value is corresponded to the selected value 
+        differentElementsPage.checkLoggingOfDropDownMenu(YELLOW);
+
+        //17 Unselect and assert checkboxes
+        differentElementsPage.unselectCheckBoxes(WATER, WIND);
+
+        //18 Assert that for each checkbox there is an individual log row and value is corresponded to the status of checkbox
+        differentElementsPage.checkLoggingOfCheckBoxesUnchecked(WATER, WIND);
+    }
+}
