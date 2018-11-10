b3a0b95708f542493ecbbedd0ef46d5da7808d04
Mikhail Zheltyshev <mihalich93@bk.ru>
2018-11-10 19:12:10 +0300

    HW5 optimizations

--- MikhailZheltyshev/src/main/java/base/SelenideTestBase.java
+++ MikhailZheltyshev/src/main/java/base/SelenideTestBase.java
@@ -5,8 +5,9 @@ import org.testng.annotations.BeforeSuite;
 
 public class SelenideTestBase {
 
-    @BeforeSuite(alwaysRun = true)
+    @BeforeSuite
     public void beforeSuite() {
         Configuration.browser = "chrome";
     }
+
 }
